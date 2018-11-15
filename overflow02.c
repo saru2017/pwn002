c2c74e3555e4a8de6b0dbcdd7fbf9681134fc30d
saru <saru@ist.osaka-u.ac.jp>
2018-11-15 15:45:46 +0000

    finished this section.

--- overflow02.c
+++ overflow02.c
@@ -5,27 +5,20 @@
 void flag()
 {
   puts("flag is HANDAI_CTF");
+  fflush(stdout);
+//  sleep(1);
 }
 
 void saru()
 {
   char buf[16];
-//  int dummy = 0x1234;
-//  int secret = 0;
-  puts("hello world");
 
   gets(buf);
-//  printf("buf = %p\n", buf);
-//  printf("dummy = %p\n", &dummy);
-//  printf("secret = %p\n", &secret);
 }
 
 int main(){
   saru();
 
-  printf("main = %p\n", &main);
-  printf("saru = %p\n", &saru);
-  printf("flag = %p\n", &flag);
   return 0;
 }
 
