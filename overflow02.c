3811a09ee9c6e5a7232fb44e51058faebe9ce61d
saru <saru@ist.osaka-u.ac.jp>
2018-11-15 09:34:41 +0000

    succeeded to stdout overflow.

--- overflow02.c
+++ overflow02.c
@@ -10,17 +10,22 @@ void flag()
 void saru()
 {
   char buf[16];
-  int dummy = 0x1234;
-  int secret = 0;
+//  int dummy = 0x1234;
+//  int secret = 0;
+  puts("hello world");
 
   gets(buf);
-
-  printf("buf[] = %s\n", buf);
+//  printf("buf = %p\n", buf);
+//  printf("dummy = %p\n", &dummy);
+//  printf("secret = %p\n", &secret);
 }
 
 int main(){
   saru();
 
+  printf("main = %p\n", &main);
+  printf("saru = %p\n", &saru);
+  printf("flag = %p\n", &flag);
   return 0;
 }
 
